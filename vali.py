591c1bb53b5779c4dc463a9babf46239acf2d406
Tran Minh <teemo2105@gmail.com>
2023-07-20 10:20:47 +0700

    done

--- vali.py
+++ vali.py
@@ -6,8 +6,8 @@ import re
 def load_problem(filename):
     # Đọc file sử dụng pickle và gzip
     with gz.open(filename, 'rb') as f:
-        problem_file = pkl.load(f)
-    return problem_file
+        problem_data = pkl.load(f)
+    return problem_data
 
 def load_solution(solution_file):
     # Đọc file kết quả
@@ -29,7 +29,7 @@ def load_solution(solution_file):
             sfc, link_S, link = match
             if sfc not in phiLink_values:
                 phiLink_values[sfc] = {}
-            phiLink_values[sfc][(link_S, link)] = int(value)
+            phiLink_values[sfc][(int(link_S), int(link))] = int(value)
         elif line.startswith("phiNode_s"):
             # Tách tên biến và giá trị
             variable_name, value, _ = line.split()
@@ -37,7 +37,7 @@ def load_solution(solution_file):
             sfc, node_S, node = variable_name.split('_')[1:]
             if sfc not in phiNode_values:
                 phiNode_values[sfc] = {}
-            phiNode_values[sfc][node_S] = int(value)
+            phiNode_values[sfc][int(node_S)] = int(value)
     
     return phiNode_values, phiLink_values
 
@@ -58,9 +58,9 @@ def validate_solution(phiNode_values, phiLink_values, G, SFCs):
     # Kiểm tra ràng buộc C2
     for edge in G.edges:
         sum_requirements = sum(
-            phiLink_values.get(sfc, {}).get(link_S, {}).get(edge, 0) * nx.get_edge_attributes(sfc, "Requirement").get(link_S, 0)
+            phiLink_values.get(sfc, {}).get(link_S, {}).get(edge, 0) * nx.get_edge_attributes(G, "Requirement").get(edge, 0)
             for sfc in SFCs
-            for link_S in sfc["edges"]
+            for link_S, link in sfc["edges"]
         )
         capacity = nx.get_edge_attributes(G, "Capacity").get(edge, 0)
         if sum_requirements > capacity:
@@ -94,22 +94,24 @@ def validate_solution(phiNode_values, phiLink_values, G, SFCs):
 
     # Kiểm tra ràng buộc C5
     for sfc in SFCs:
-        for edge_S in sfc["edges"]:
+        for edge_S, edge in sfc["edges"]:
             for node in G.nodes:
                 sum_phiLink_out = sum(
-                    phiLink_values.get(sfc, {}).get(edge_S, {}).get((node, nodej), 0)
-                    for nodej in G.nodes
+                    phiLink_values.get(sfc, {}).get((link_S, link), 0)
+                    for link_S, link in sfc["edges"]
+                    if (node, link) in phiLink_values.get(sfc, {})
                 )
                 sum_phiLink_in = sum(
-                    phiLink_values.get(sfc, {}).get(edge_S, {}).get((nodej, node), 0)
-                    for nodej in G.nodes
+                    phiLink_values.get(sfc, {}).get((link_S, link), 0)
+                    for link_S, link in sfc["edges"]
+                    if (link, node) in phiLink_values.get(sfc, {})
                 )
-                sum_phiNode_out = phiNode_values.get(sfc, {}).get(edge_S[0], {}).get(node, 0)
-                sum_phiNode_in = phiNode_values.get(sfc, {}).get(edge_S[1], {}).get(node, 0)
+                sum_phiNode_out = phiNode_values.get(sfc, {}).get(edge_S, 0)
+                sum_phiNode_in = phiNode_values.get(sfc, {}).get(edge, 0)
 
                 if sum_phiLink_out - sum_phiLink_in != sum_phiNode_out - sum_phiNode_in:
                     # Ràng buộc không được thỏa mãn
-                    print(f"C5_s{SFCs.index(sfc)}_vw{edge_S}_i{node} không được thỏa mãn")
+                    print(f"C5_s{SFCs.index(sfc)}_vw{edge_S}_{edge}_i{node} không được thỏa mãn")
                     return False
 
     # Tất cả các ràng buộc được thỏa mãn
@@ -119,8 +121,7 @@ def validate_solution(phiNode_values, phiLink_values, G, SFCs):
 def validate(problem_file, solution_file):
     # Load thông tin từ tệp problem
     problem_data = load_problem(problem_file)
-    G = problem_data["PHYGraph"]
-    SFCs = problem_data["SFCsList"]
+    G, SFCs = problem_data
     
     # Load giải pháp từ tệp solution
     phiNode_values, phiLink_values = load_solution(solution_file)
@@ -133,7 +134,7 @@ def validate(problem_file, solution_file):
     else:
         print("Có ràng buộc không được thỏa mãn")
 
-if __name__=="__main__":
-    problem_file = "demo_multiple_mapping_1.dat"
-    solution_file = "demo_multiple_mapping_4.mps.sol"
+if __name__ == "__main__":
+    problem_file = r"..\vali\demo_multiple_mapping_1.dat"
+    solution_file = r"..\vali\demo_multiple_mapping_1.mps.sol"
     validate(problem_file, solution_file)
